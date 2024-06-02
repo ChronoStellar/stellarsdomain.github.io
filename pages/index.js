7d68f6ef0e231a85580228df24001720d41a7f12
ChronoStellar <hendrikcarlo0@gmail.com>
2024-06-02 20:47:00 +0700

    Add dark mode

--- pages/index.js
+++ pages/index.js
@@ -26,8 +26,8 @@ export default function Home() {
         <meta name="description" content="Welcome to the portfolio of Hendrik Nicolas, showcasing projects and skills." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className="bg-slate-700 dark:bg-white">
-        <nav className="py-10 px-8 flex justify-between text-white">
+      <header className="bg-slate-700 dark:bg-slate-200">
+        <nav className="py-10 px-8 flex justify-between text-white dark:text-slate-700">
             <h1 className="text-xl flex items-center">
               <SiStellar className="inline-block mr-2" /> Stellar's Domain
             </h1>
@@ -35,13 +35,13 @@ export default function Home() {
               <li>
                 <SiStarship
                   onClick={() => setDarkMode(!darkMode)}
-                  className=" cursor-pointer text-2xl"
+                  className=" cursor-pointer text-2xl hover:text-black dark:hover:text-white"
                 />
               </li>
               <li>
                 <a
-                  className="bg-gradient-to-r from-rose-500 text- to-red-600  text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="https://www.canva.com/design/DAGGszsI_rg/1tBB1VZ0FqksMuluVPcuHA/view?utm_content=DAGGszsI_rg&utm_campaign=designshare&utm_medium=link&utm_source=editor">
+                  className="bg-gradient-to-r from-rose-500 text- to-red-600 hover:to-rose-700 text-white px-4 py-2 border-none rounded-md ml-8 dark:from-amber-400 dark:to-yellow-600 dark:hover:to-amber-300 dark:text-slate-700"
+                  href="https://www.canva.com/design/DAGGszsI_rg/1tBB1VZ0FqksMuluVPcuHA/view?utm_content=DAGGszsI_rg&utm_campaign=designshare&utm_medium=link&utm_source=editor" target="_blank">
                   Resume
                 </a>
               </li>
@@ -49,13 +49,13 @@ export default function Home() {
           </nav>
       </header>
       
-      <main className="bg-gray-100 h-auto">
+      <main className="bg-gray-100 h-auto dark:text-gray-100 dark:bg-black">
         <section className="">
           <div className="text-center p-10 py-10">
-          <h2 className="text-5xl py-2 hover:text-red-400 transition-colors text-yellow-400 font-medium dark:text-yellow-100 md:text-6xl">
+          <h2 className="text-5xl py-2 hover:text-red-400 transition-colors text-yellow-400 font-medium dark:hover:text-yellow-200 dark:text-rose-700 md:text-6xl">
             Hendrik Nicolas Carlo
           </h2>
-          <h3 className="text-2xl py-2 dark:text-black md:text-3xl">
+          <h3 className="text-2xl py-2 md:text-3xl">
             AI developer & Aspiring Data Scientist
           </h3>
           <div className="mx-auto rounded-full w-80 h-80 relative overflow-hidden mt-10 md:h-96 md:w-96">
@@ -82,15 +82,15 @@ export default function Home() {
         </section>
         <section className="p-10 " >
           <div className="text-justify">
-          <h2 className="text-5xl py-2 text-yellow-400 font-medium dark:text-yellow-500 md:text-6xl">
+          <h2 className="text-5xl py-2 text-yellow-400 font-medium dark:text-rose-700 md:text-6xl">
             My Projects
           </h2>
           <p className="text-xl">
-            These are the projects I've made for personal Projects and Competitions. I didn't include my python notebooks here. Those sonsist of mostly Deep Learning models and transformers. You can see them in my github page.
+            These are the projects I've made for personal Projects and Competitions. I didn't include my python notebooks here. Those consist of mostly Deep Learning models and transformers. You can see them in my github page.
           </p>
           </div>
           <div className="flex-row flex-wrap gap-5 overflow-auto items-baseline w-auto">
-            <div className="flex flex-col sm:flex-row  text-center shadow-lg my-5 dark:bg-white flex-1 border-black">
+            <div className="flex flex-col sm:flex-row  text-center shadow-lg my-5 dark:shadow-gray-700 flex-1">
               <div className="p-2 h-full w-full sm:w-6/12">
                 <Image
                   className="rounded-lg object-cover"
@@ -107,7 +107,7 @@ export default function Home() {
                 </p>
               </div>
             </div>
-            <div className="flex flex-col sm:flex-row text-center shadow-lg my-5 dark:bg-white flex-1 border-black">
+            <div className="flex flex-col sm:flex-row  text-center shadow-lg my-5 dark:shadow-gray-700 flex-1">
               <div className="align-middle text-justify p-5 w-full sm:w-3/6">
                 <p className="text-xl">
                   A collaborative project for a hackathon. This mobile app is made with react native and microsoft azure AI service for speech. The Idea is that by using sounds and vibration user can learn braille even if they have visual impairment. I was resposible for the management, concepts, reports and the sort. While I didn't help with the coding much since the time was limited, I helped with making the AI API.
@@ -130,7 +130,7 @@ export default function Home() {
                 />
               </div>
             </div>
-            <div className="flex flex-col sm:flex-row  text-center shadow-lg my-5 dark:bg-white flex-1 border-black">
+            <div className="flex flex-col sm:flex-row  text-center shadow-lg my-5 dark:shadow-gray-700 flex-1">
               <div className="p-2 h-full w-full sm:w-6/12">
                 <Image
                   className="rounded-lg object-cover"
@@ -149,30 +149,30 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section className="bg-blue-100 min-h-screen">
+        <section className="bg-blue-100 min-h-screen dark:bg-cyan-950">
           <div className="text-center p-5 flex flex-col gap-5 lg:flex-row lg:items-start lg:justify-center lg:py-10">
-            <div className="bg-green-50 w-full lg:w-8/12 p-5 lg:p-10">
+            <div className="bg-green-100 dark:bg-emerald-900 w-full lg:w-8/12 p-5 lg:p-10">
               <h2 className="font-bold text-4xl">About Me:</h2>
               <p className="text-justify px-2 sm:px-10 pt-5 text-xl">
                 Hello! It's a pleasure meeting you, my name is Hendrik Nicolas Carlo, and I am a computer science undergraduate from Binus University. I've been coding since High School and I'm quite passionate about it. I mostly specialize in AI development, where I have honed my skills in Python. However, I also dabble in web development and software development, hence this site is made with Next.js. In my free time, I love to play Monster Hunter, build model kits, and such other hobbies.
                 <br />I am excited to bring my skills and self to new challenges and opportunities. Once Again, Nice to meet you!
               </p>
             </div>
-            <div className="bg-amber-100 w-full lg:w-5/12 p-5 lg:p-10 text-left">
+            <div className="bg-amber-100 dark:bg-amber-900 w-full lg:w-5/12 p-5 lg:p-10 text-left">
               <div>
                 <h3 className="text-2xl font-bold">Contact me:</h3>
                 <ol className="pt-2 text-xl">
-                  <li><a href="mailto:hendrik.carlo@binus.co.id">hendrik.carlo@binus.ac.id</a></li>
-                  <li>(+62) 878 2487 6058</li>
-                  <li><a href="https://www.linkedin.com/in/hendrik-nicolas-carlo-95ba3a275/"><IoLogoLinkedin className="inline"/> Linkedin</a></li>
-                  <li><a href="https://github.com/ChronoStellar"><IoLogoGithub className="inline"/> ChronoStellar</a></li>
+                  <li className="hover:font-bold"><a href="mailto:hendrik.carlo@binus.co.id">hendrik.carlo@binus.ac.id</a></li>
+                  <li className="hover:font-bold">(+62) 878 2487 6058</li>
+                  <li className="hover:font-bold"><a href="https://www.linkedin.com/in/hendrik-nicolas-carlo-95ba3a275/" target="_blank"><IoLogoLinkedin className="inline"/> Linkedin</a></li>
+                  <li className="hover:font-bold"><a href="https://github.com/ChronoStellar" target="_blank"><IoLogoGithub className="inline"/> ChronoStellar</a></li>
                 </ol>
               </div>
             </div>
           </div>
         </section>
       </main>
-      <footer className="bg-slate-700 p-10 text-slate-50 text-center w-full">
+      <footer className="bg-slate-700 p-10 text-slate-50 text-center w-full dark:bg-slate-200 dark:text-slate-700">
         <p>&copy; 2024 Hendrik Nicolas. All rights reserved.</p>
       </footer>
     </div>
